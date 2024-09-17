60516fba4dd4dc94d3d0c51c0750d9b3f913bbb2
Taylor Webb <tbwebb22@gmail.com>
2024-09-17 14:54:16 -0400

    format

--- contracts/LiquidityToken.sol
+++ contracts/LiquidityToken.sol
@@ -4,9 +4,8 @@ pragma solidity =0.8.24;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit, Nonces} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract LiquidityToken is ERC20Permit, ERC20Votes, ERC20Burnable {
+contract LiquidityToken is ERC20Permit, ERC20Votes {
     constructor(
         string memory name_,
         string memory symbol_,
